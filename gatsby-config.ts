0c517c06d7ae63012778167d9dd6831fcd703f1a
oktapascal <oktaiscool@gmail.com>
2023-10-11 15:39:30 +0700

    first commit

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,21 +2,27 @@ import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `mysai`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: "PT. Samudra Aplikasi Indonesia",
+    siteUrl: "https://www.yourdomain.tld",
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-transformer-remark", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
+  plugins: [
+    "gatsby-transformer-remark",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
     },
-    __key: "pages"
-  }]
+  ],
 };
 
 export default config;
